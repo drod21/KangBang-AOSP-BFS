f360976e79d40c5ae3c6c7f7b8858f78059fbd87
Derek Rodriguez <drod2169@gmail.com>
2011-05-31 19:46:16 -0400

    Wish I did this a simpler way..

--- drivers/net/wireless/bcm4329/.bcm4329.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_drivers/net/wireless/bcm4329/bcm4329.ko := /home/drod2169/cm/prebuilt/linux-x86/toolchain/arm-eabi-4.4.3/bin/arm-eabi-ld -EL -r  -T /home/drod2169/KangBang-AOSP-BFS/scripts/module-common.lds --build-id -o drivers/net/wireless/bcm4329/bcm4329.ko drivers/net/wireless/bcm4329/bcm4329.o drivers/net/wireless/bcm4329/bcm4329.mod.o
