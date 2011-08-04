829bbae4e6f04167c1d07b81e85a263fbea251cb
Derek Rodriguez <drod2169@gmail.com>
2011-08-04 11:27:28 -0400

    Don't remember what I changed here, will update when I see the code changes

--- arch/arm/mach-msm/board-mecha-wifi.c
+++ arch/arm/mach-msm/board-mecha-wifi.c
@@ -86,7 +86,6 @@ static struct wifi_platform_data mecha_wifi_control = {
 	.set_reset      = mecha_wifi_reset,
 	.set_carddetect = mecha_wifi_set_carddetect,
 	.mem_prealloc   = mecha_wifi_mem_prealloc,
-    .dot11n_enable  = 1,
 };
 
 static struct platform_device mecha_wifi_device = {
